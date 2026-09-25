0080f73eeaef7ed42378677337b4c02d32b663c0
Galang Pratama <galangpratama668@gmail.com>
2026-09-25 23:14:03 +0700

    some update erd

--- app/siakad-kel3.sql
+++ app/siakad-kel3.sql
@@ -2,18 +2,6 @@ CREATE DATABASE IF NOT EXISTS `siakad-kel3`;
 
 USE `siakad-kel3`;
 
-CREATE TABLE siswa (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(100) NOT NULL,
-    nis VARCHAR(20) NOT NULL UNIQUE,
-    nisn VARCHAR(20) NOT NULL UNIQUE,
-    jenis_kelamin ENUM("L", "P") NOT NULL,
-    alamat TEXT NOT NULL,
-    tanggal_lahir DATE NOT NULL,
-    no_telepon VARCHAR(20) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-);
-
 -- INSERT INTO siswa (
 --     nama, nis, nisn, jenis_kelamin, alamat, tanggal_lahir, no_telepon, email
 --     )
@@ -26,7 +14,7 @@ CREATE TABLE siswa (
 CREATE TABLE guru (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama VARCHAR(100) NOT NULL,
-    nip INT NOT NULL UNIQUE,
+    nip VARCHAR(30) NOT NULL UNIQUE,
     jenis_kelamin ENUM("L", "P") NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     no_telepon VARCHAR(20) NOT NULL
@@ -43,18 +31,28 @@ CREATE TABLE guru (
 --         "Imron", "186347648", "L", "imron@anuan.id", "08137683653"
 --     );
 
+CREATE TABLE jurusan (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama_jurusan VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE modul (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama_modul VARCHAR(100) NOT NULL
+);
+
 
 CREATE TABLE kelas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama_kelas VARCHAR(20) NOT NULL,
     tingkat INT NOT NULL,
     jurusan_id INT NOT NULL,
-    guru_id INT NOT NULL
+    guru_id INT NOT NULL,
 
     FOREIGN KEY (jurusan_id)
         REFERENCES jurusan(id)
         ON DELETE RESTRICT
-        ON UPDATE CASCADE
+        ON UPDATE CASCADE,
 
     FOREIGN KEY (guru_id)
         REFERENCES guru(id)
@@ -62,21 +60,34 @@ CREATE TABLE kelas (
         ON UPDATE CASCADE
 );
 
-CREATE TABLE jurusan (
+CREATE TABLE siswa (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nama_jurusan VARCHAR(20) NOT NULL
+    nama VARCHAR(100) NOT NULL,
+    nis VARCHAR(20) NOT NULL UNIQUE,
+    nisn VARCHAR(20) NOT NULL UNIQUE,
+    jenis_kelamin ENUM("L", "P") NOT NULL,
+    alamat TEXT NOT NULL,
+    tanggal_lahir DATE NOT NULL,
+    no_telepon VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    kelas_id INT NOT NULL,
+
+    FOREIGN KEY (kelas_id)
+        REFERENCES kelas(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE mata_pelajaran (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nama_mapel VARCHAR(20) NOT NULL,
-    guru_id INT NOT NULL,
     modul_id INT NOT NULL,
+    guru_id INT NOT NULL,
 
     FOREIGN KEY (guru_id)
         REFERENCES guru(id)
         ON DELETE RESTRICT
-        ON UPDATE CASCADE
+        ON UPDATE CASCADE,
 
     FOREIGN KEY (modul_id)
         REFERENCES modul(id)
@@ -99,17 +110,35 @@ CREATE TABLE jadwal (
     id INT AUTO_INCREMENT PRIMARY KEY,
     mata_pelajaran_id INT NOT NULL,
     guru_id INT NOT NULL,
-    hari DATE NOT NULL,
-    jam VARCHAR(10) NOT NULL,
+    kelas_id INT NOT NULL,
+    semester_id INT NOT NULL,
+    tahun_ajaran_id INT NOT NULL,
+    hari ENUM('Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu') NOT NULL,
+    jam_mulai TIME NOT NULL,
+    jam_selesai TIME NOT NULL,
 
     FOREIGN KEY (guru_id)
         REFERENCES guru(id)
         ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (kelas_id)
+        REFERENCES kelas(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (semester_id)
+        REFERENCES semester(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (tahun_ajaran_id)
+        REFERENCES tahun_ajaran(id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (mata_pelajaran_id)
+        REFERENCES mata_pelajaran(id)
+        ON DELETE RESTRICT
         ON UPDATE CASCADE
 );
-
-CREATE TABLE modul (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama_modul VARCHAR(100) NOT NULL,
-);
-
